79a4e3d22b18b4b41a99821db7464bafb880ee0a
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-07-04 14:48:54 +0300

    Contains Dublicate

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] nums = {1,2,4,8,2 };
+
+Console.WriteLine(ContainsDublicate(nums));
+
+
+
+
+bool ContainsDublicate(int[]nums)
+{
+    HashSet<int> set = new HashSet<int>();
+
+    foreach (var item in nums)
+    {
+        if (set.Contains(item))
+            return true;
+
+        set.Add(item);
+    }
+
+    return false;
+}
